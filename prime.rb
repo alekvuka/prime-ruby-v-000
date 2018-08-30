787ce7073b9c7de4f04e5a37b29424cf002e6e1d
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-30 00:31:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+
+#
+#def prime?(number)
+#  if number % 2 == 0
+#    return false
+#  else
+#    return true
+#  end
+#end
+#
+
+def prime?(number)
+  if number <= 1
+    return false
+  else
+    i = 1
+    while i < number
+      if number % i == 0
+        return false
+      end
+    end
+  end 
